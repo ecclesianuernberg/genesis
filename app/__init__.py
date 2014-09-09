c4feeed10ed83a04bbc1e5616ef62f596d8ff771
Marvin Steadfast <xsteadfastx@gmail.com>
2014-09-09 14:36:35 +0200

    flask-admin

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 import config
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
 db = SQLAlchemy(app)
-from . import views
-from . import models
+import views
+import models
+admin = Admin(app)
+admin.add_view(ModelView(models.User, db.session))

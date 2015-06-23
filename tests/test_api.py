e7928534f1d8eb2e534047ff68aaf948bb530082
Marvin Steadfast <marvin@xsteadfastx.org>
2015-06-23 11:43:09 +0000

    splitted test files
    the tests used to be a very large file with all fixtures and helpers
    included. i always wanted to split things up. right now i seperate api,
    app and churchtool tests plus all fixture went already into conftest.py.
    makes everything nicer now.

---
+++ tests/test_api.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+import pytest
+import app
+import json
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer)
+from app import ct_connect
+from helper import (create_api_creds, get_api_token, add_prayer_api,
+                    edit_prayer_api, del_prayer_api, group_overview_api,
+                    group_item_api)
+
+
+# get test user
+TEST_USER = app.app.config['TEST_USER']
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_token(client, test_user):
+    ''' get valid token '''
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    rv = get_api_token(client, creds)
+
+    assert rv.status_code == 200
+
+    token = json.loads(rv.data)['token']
+    s = Serializer(app.app.config['SECRET_KEY'])
+
+    assert s.loads(token)['id'] == test_user['email']
+
+    # wrong password
+    creds = create_api_creds(test_user['email'], 'wrongpassword')
+    rv = get_api_token(client, creds)
+
+    assert rv.status_code == 401
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_add_prayer(client, test_user):
+    ''' add prayer through api '''
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    body = 'Test'
+    name = 'Testname'
+
+    rv = add_prayer_api(client, body, creds, name)
+
+    assert rv.status_code == 200
+
+    # create dict out of json response
+    data_dict = json.loads(rv.data)
+
+    assert data_dict.get('name') == name
+    assert data_dict.get('id') == 1
+    assert data_dict.get('prayer') == 'Test'
+
+    # wrong password
+    creds = create_api_creds(test_user['email'], 'wrongpassword')
+    rv = add_prayer_api(client, body, creds, name)
+
+    assert rv.status_code == 401
+
+    # check db entry
+    db_entry = app.models.get_prayer(1)
+    assert db_entry.id == 1
+    assert db_entry.body == body
+    assert db_entry.user_id == test_user['id']
+    assert db_entry.active is True
+    assert db_entry.name == name
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_add_prayer_token(client, test_user):
+    ''' add prayer through api with a auth token '''
+    # get token
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    rv = get_api_token(client, creds)
+    token = json.loads(rv.data)['token']
+
+    # auth with token
+    creds = create_api_creds(token, 'foo')
+    body = 'Test'
+    rv = add_prayer_api(client, body, creds)
+
+    assert rv.status_code == 200
+
+    # create dict out of json response
+    data_dict = json.loads(rv.data)
+
+    assert data_dict.get('name') == ''
+    assert data_dict.get('id') == 1
+    assert data_dict.get('prayer') == 'Test'
+
+    # wrong token
+    token = 'wrongtoken'
+    creds = create_api_creds(token, 'foo')
+    rv = add_prayer_api(client, body, creds)
+
+    assert rv.status_code == 401
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_edit_prayer(client, test_user):
+    ''' edit prayer through api '''
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    body = 'Noch ein Test'
+    active = True
+    name = 'Testname'
+
+    # add prayer
+    add_prayer_api(client, 'Test', creds)
+
+    # edit prayer
+    rv = edit_prayer_api(client, 1, body, creds, active, name)
+
+    assert rv.status_code == 200
+
+    # create dict out of json response
+    data_dict = json.loads(rv.data)
+
+    assert data_dict.get('name') == name
+    assert data_dict.get('id') == 1
+    assert data_dict.get('prayer') == 'Noch ein Test'
+
+    # wrong prayer
+    rv = edit_prayer_api(client, 20, body, creds, active, name)
+
+    assert rv.status_code == 404
+
+    # wrong password
+    creds = create_api_creds(test_user['email'], 'wrongpassword')
+    body = 'Test'
+    rv = edit_prayer_api(client, 1, body, creds, active, name)
+
+    assert rv.status_code == 401
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_del_prayer(client, test_user):
+    ''' delete prayer through api '''
+    creds = create_api_creds(test_user['email'], test_user['password'])
+
+    # add prayer
+    add_prayer_api(client, 'Test', creds)
+
+    rv = del_prayer_api(client, 1, creds)
+    assert rv.status_code == 204
+
+    rv = del_prayer_api(client, 20, creds)
+    assert rv.status_code == 404
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_group_overview_authorized(client, test_user):
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    rv = group_overview_api(client, creds)
+
+    assert rv.status_code == 200
+
+    with ct_connect.session_scope() as ct_session:
+        assert len(ct_connect.get_active_groups(ct_session)) == len(
+            json.loads(rv.data))
+
+        for group in json.loads(rv.data):
+            group_ct = ct_connect.get_group(ct_session, group['id'])
+            group_metadata = app.models.get_group_metadata(group['id'])
+
+            assert group['name'] == group_ct.bezeichnung.split(' - ')[-1]
+            assert group['id'] == group_ct.id
+            assert group['treffzeit'] == group_ct.treffzeit
+            assert group['treffpunkt'] == group_ct.treffpunkt
+            assert group['zielgruppe'] == group_ct.zielgruppe
+            assert group['notiz'] == group_ct.notiz
+
+            if group['avatar']:
+                assert group['avatar'] == group_metadata.avatar_id
+
+            if group['description']:
+                assert group['description'] == group_metadata.description
+
+
+def test_group_overview_unauthorized(client):
+    rv = client.get('/api/groups')
+
+    assert rv.status_code == 200
+
+    with ct_connect.session_scope() as ct_session:
+        assert len(ct_connect.get_active_groups(ct_session)) == len(
+            json.loads(rv.data))
+
+        for group in json.loads(rv.data):
+            group_ct = ct_connect.get_group(ct_session, group['id'])
+            group_metadata = app.models.get_group_metadata(group['id'])
+
+            assert group['name'] == group_ct.bezeichnung.split(' - ')[-1]
+            assert group['id'] == group_ct.id
+            assert group['treffzeit'] == group_ct.treffzeit
+            assert group['treffpunkt'] == ''
+            assert group['zielgruppe'] == group_ct.zielgruppe
+            assert group['notiz'] == group_ct.notiz
+
+            if group['avatar']:
+                assert group['avatar'] == group_metadata.avatar_id
+
+            if group['description']:
+                assert group['description'] == group_metadata.description
+
+
+def test_group_overview_wrong_password(client):
+    creds = create_api_creds(TEST_USER[0]['email'], 'wrong')
+    rv = group_overview_api(client, creds)
+
+    assert rv.status_code == 200
+
+    with ct_connect.session_scope() as ct_session:
+        assert len(ct_connect.get_active_groups(ct_session)) == len(
+            json.loads(rv.data))
+
+        for group in json.loads(rv.data):
+            group_ct = ct_connect.get_group(ct_session, group['id'])
+            group_metadata = app.models.get_group_metadata(group['id'])
+
+            assert group['name'] == group_ct.bezeichnung.split(' - ')[-1]
+            assert group['id'] == group_ct.id
+            assert group['treffzeit'] == group_ct.treffzeit
+            assert group['treffpunkt'] == ''
+            assert group['zielgruppe'] == group_ct.zielgruppe
+            assert group['notiz'] == group_ct.notiz
+
+            if group['avatar']:
+                assert group['avatar'] == group_metadata.avatar_id
+
+            if group['description']:
+                assert group['description'] == group_metadata.description
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_group_item(client, test_user):
+    # unauthorized
+    rv = client.get('/api/group/1')
+
+    assert rv.status_code == 401
+
+    # wrong password
+    creds = create_api_creds(test_user['email'], 'wrong')
+    rv = group_item_api(client, creds, 1)
+
+    assert rv.status_code == 401
+
+    # valid userdata
+    creds = create_api_creds(test_user['email'], test_user['password'])
+    rv = group_item_api(client, creds, 1)
+
+    assert rv.status_code == 200
+
+    rv_json = json.loads(rv.data)
+
+    with ct_connect.session_scope() as ct_session:
+        group_ct = ct_connect.get_group(ct_session, 1)
+        group_metadata = app.models.get_group_metadata(1)
+
+        assert rv_json['name'] == group_ct.bezeichnung.split(' - ')[-1]
+        assert rv_json['id'] == group_ct.id
+
+        if rv_json['description']:
+            assert rv_json['description'] == group_metadata.description
+
+        assert rv_json['treffzeit'] == group_ct.treffzeit
+        assert rv_json['treffpunkt'] == group_ct.treffpunkt
+        assert rv_json['zielgruppe'] == group_ct.zielgruppe
+        assert rv_json['notiz'] == group_ct.notiz
+
+        if rv_json['avatar']:
+            assert rv.json['avatar'] == group_metadata.avatar_id
---
+++ tests/test_ct.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import pytest
+import app
+from app import ct_connect
+from datetime import datetime
+from passlib.hash import bcrypt
+
+
+# get test user
+TEST_USER = app.app.config['TEST_USER']
+
+
+@pytest.mark.parametrize('test_user', TEST_USER[1:3])
+def test_get_person(test_user):
+    ''' person out of churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_person(ct_session, test_user['email'])
+
+        assert test_user['name'] in [i.name for i in rv]
+        assert test_user['vorname'] in [i.vorname for i in rv]
+
+
+@pytest.mark.parametrize('test_user', TEST_USER[1:3])
+def test_person_from_id(test_user):
+    ''' person out of churchtools from id '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_person_from_id(ct_session, test_user['id'])[0]
+
+        assert rv.email == test_user['email']
+        assert rv.name == test_user['name']
+        assert rv.vorname == test_user['vorname']
+
+
+def test_get_active_groups():
+    ''' list of active groups out of churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_active_groups(ct_session)
+
+        assert u'KG - Test' in [i.bezeichnung for i in rv]
+
+
+def test_get_group():
+    ''' specific group out of churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_group(ct_session, 1)
+
+        assert rv.valid_yn == 1
+        assert rv.versteckt_yn == 0
+        assert rv.bezeichnung == u'KG - Test'
+        assert rv.gruendungsdatum == datetime(2010, 0o4, 0o6, 00, 00, 00)
+        assert rv.treffzeit == u'Jeden Mittwoch 19h'
+        assert rv.treffpunkt == u'Siebenkeesstr. 18, 90459 Nürnberg'
+        assert rv.zielgruppe == u'Zwischen 9-17J.'
+        assert rv.gruppentyp_id == 1
+        assert rv.distrikt_id == 14
+        assert rv.geolat == u'49.4412072'
+        assert rv.geolng == u'11.078397799999948'
+        assert rv.offen_yn == 0
+        assert rv.oeffentlich_yn == 0
+        assert rv.treffen_yn == 1
+        assert rv.instatistik_yn == 1
+        assert rv.mail_an_leiter_yn == 1
+        assert rv.members_allowedmail_eachother_yn == 0
+        assert rv.followup_typ_id == 0
+        assert rv.fu_nachfolge_typ_id == 0
+        assert rv.fu_nachfolge_objekt_id == 0
+        assert rv.fu_nachfolge_gruppenteilnehmerstatus_id == 0
+
+
+def test_get_group_heads():
+    ''' list of group heads out of churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_group_heads(ct_session, 1)
+
+        assert [u'Leiter', u'Preuß'] == [i.name for i in rv]
+
+
+def test_get_group_members():
+    ''' group members out of churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_group_members(ct_session, 1)
+
+        assert [118, 163, 383] == [i.id for i in rv]
+
+
+def test_get_person_from_communityperson():
+    ''' person object out of communityperson from churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        rv = ct_connect.get_person_from_communityperson(ct_session, 118)
+
+        assert rv.name == u'Leiter'
+        assert rv.vorname == u'Test'
+        assert rv.email == u'test.leiter@ecclesianuernberg.de'
+
+
+@pytest.mark.parametrize('test_user', TEST_USER)
+def test_change_user_password(test_user):
+    ''' change user password in churchtools '''
+    with ct_connect.session_scope() as ct_session:
+        # check if test_user password is the one in the churchtools-db
+        db_user = ct_connect.get_person_from_id(ct_session, test_user['id'])[0]
+
+        assert bcrypt.verify(test_user['password'], db_user.password)
+
+        # new password
+        password = 'newpassword'
+        ct_connect.change_user_password(ct_session, test_user['id'], password)
+        db_user = ct_connect.get_person_from_id(ct_session, test_user['id'])[0]
+
+        assert bcrypt.verify(password, db_user.password)
+
+        # reset password
+        ct_connect.change_user_password(ct_session, test_user['id'],
+                                        test_user['password'])
